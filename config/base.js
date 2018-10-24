6cab988a210dd5d7b2a9f2882d98b8e6341532a1
cdzhengwujiang <cdzhengwujiang@jd.com>
2018-10-24 13:33:59 +0800

    Update package.json

--- config/base.js
+++ config/base.js
@@ -34,7 +34,7 @@ module.exports = {
                     loader: "babel-loader",
                     options: {
                         presets: [
-                            "es2015", "react", "stage-0"
+                            "env", "react", "stage-0"
                         ]
                     }
                 },
